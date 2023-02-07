ed37f9bb3e52f12d3e35b154564234d460e2be8c
daichi_yamaoka <tnychor06@gmail.com>
2023-02-08 00:06:42 +0900

    update

--- src/components/GolfCourseTour.tsx
+++ src/components/GolfCourseTour.tsx
@@ -394,7 +394,7 @@ const GolfCourseTourComponent: NextPage = () => {
     document.addEventListener('touchmove', (event) => {
       var currentY = event.touches[0].clientY
       const delta = currentY - lastY
-      scrollDistance += delta * 0.3
+      scrollDistance += delta * 0.5
       event.preventDefault()
     })
     return () => {
